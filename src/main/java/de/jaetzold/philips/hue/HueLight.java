ba90fd6e0840f83ea056d2e8d331a7911a61cf1b
Stephan Jaetzold <stephan@jaetzold.de>
2013-04-01 13:24:28 +0200

    Add licensing information

--- src/main/java/de/jaetzold/philips/hue/HueLight.java
+++ src/main/java/de/jaetzold/philips/hue/HueLight.java
@@ -1,5 +1,18 @@
 package de.jaetzold.philips.hue;
 
+/*
+ Copyright (c) 2013 Stephan Jaetzold.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ */
+
 /**
  * A control interface for light devices.
  * The main purpose it serves is to be able to handle single light bulbs and whole groups of them in an interchangeable fashion.
